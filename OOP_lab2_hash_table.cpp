4bc977ed52b962c82a1b04bc537b5b419dedb388
Anastasia <imonastation@gmail.com>
2019-03-31 13:38:20 +0300

    final changes

--- OOP_lab2_hash_table.cpp
+++ OOP_lab2_hash_table.cpp
@@ -4,27 +4,33 @@ using namespace std;
 
 int main()
 {
-	string word;
-	string key = "";
+	string word = "";
+	string sentence;
 	hash_table dictionary;
 	ifstream fin("Oxford_Dictionary.txt");
 	if (fin.is_open()) {
-		cout << "Wait while the table is loading" << endl;
+		cout << "Wait while the table is loading..." << endl;
 		fill_hash_table(fin, dictionary);
 		while (true) {
-			cout << "Input your word (type 'EXIT' to leave): ";
-			getline(cin, word);
-			if (word == "EXIT") {
+			cout << "Input your sentence (type 'EXIT' to leave)" << endl;
+			getline(cin, sentence);
+			if (sentence == "EXIT") {
 				return 0;
 			}
 			else {
-				for (int i = 0; i < word.length(); i++) {
-					word[i] = tolower(word[i]);
+				sentence += ' ';
+				int count = count_words(sentence);
+				for (int i = 0; i < count; i++) {
+					word = find_word_in_a_sentence(sentence, dictionary);
+					cout << "The word is: " << word << endl;
+					cout << dictionary.find_in_dictionary(word) << endl << endl;
 				}
-				cout << dictionary.find_in_table(word) << endl << endl;
+				fin.close();
+				system("pause");
 			}
 		}
-		fin.close();
-		system("pause");
+	}
+	else {
+		cout << "unable to open file";
 	}
 }
