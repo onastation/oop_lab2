69d3bed3cabf81671f5d93232ae871d284932f3b
Anastasia <imonastation@gmail.com>
2019-03-31 13:42:09 +0300

    functions

--- functions.cpp
+++ functions.cpp
@@ -1,24 +1,47 @@
 #include "pch.h"
 #include "functions.h"
 using namespace std;
-string find_in_line(string &line) {
-	string key = "";
-	string del = "  ";
-	for (int i = 0; i < line.find(del); i++) {
-		key += line[i];
+int count_words(string sentence) {
+	int count = 0;
+	for (int i = 0; i < sentence.length(); i++) {
+		if (sentence[i] == ' ' || sentence[i] == ',') {
+			count++;
+			if (sentence[i + 1] == ' ' || sentence[i + 1] == ',') {
+				count--;
+			}
+		}
 	}
-	line.erase(0, line.find(del) + 2);
-	return key;
+	return count;
+}
+string find_word_in_a_sentence(string &sentence, hash_table &dictionary) {
+	string word = "";
+	int j = 0;
+	while (sentence[j] != ' ' && sentence[j] != ',') {
+		sentence[j] = tolower(sentence[j]);
+		word += sentence[j];
+		j++;
+	}
+	sentence.erase(0, word.length());
+	int k = 0;
+	while (sentence[k] == ' ' || sentence[k] == ',') {
+		k++;
+	}
+	sentence.erase(0, k);
+	return word;
 }
 istream& fill_hash_table(ifstream &fin, hash_table &dictionary) {
-	string key = "";
+	string word = "";
+	string definition;
 	string line;
 	while (getline(fin, line)) {
-		key = find_in_line(line);
-		for (int i = 0; i < key.length(); i++) {
-			key[i] = tolower(key[i]);
+		for (int i = 0; i < line.find("  "); i++) {
+			line[i] = tolower(line[i]);
+			word += line[i];
 		}
-		dictionary.add_term(key, line);
+		line.erase(0, word.length() + 2);
+		definition = line;
+		dictionary.add_term(word, definition);
+		word = "";
 	}
 	return fin;
 }
--- functions.h
+++ functions.h
@@ -1,4 +1,5 @@
 #include "hash_table.h"
 using namespace std;
-string find_in_line(string &line); //function that separates a word from its definition and returns that word
-istream& fill_hash_table(ifstream &fin, hash_table &dictionary); //function that parses the .txt file and fills a hash table
+int count_words(string sentence);
+string find_word_in_a_sentence(string &sentence, hash_table &dictionary);
+istream& fill_hash_table(ifstream &fin, hash_table &dictionary);
